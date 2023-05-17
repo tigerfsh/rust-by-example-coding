9005ff6a0da443b8517801308c6069b6b92ba519
fushouhai <fushouhai@haier.com>
2023-05-17 20:22:48 +0800

    add phantom type parameters

--- r14/src/main.rs
+++ r14/src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Debug, Display};
+use std::{fmt::{Debug, Display}, marker::PhantomData};
 
 struct A;
 
@@ -233,6 +233,15 @@ impl Contains for Container {
 fn difference<C: Contains>(container: &C) -> i32 {
     container.last() - container.first()
 }
+
+// phantom type parameters 
+#[derive(PartialEq)]
+struct PhantomTuple<A, B>(A, PhantomData<B>);
+
+#[derive(PartialEq)]
+struct PhantomStruct<A, B> {first: A, phantom: PhantomData<B>};
+
+
 fn main() {
     // Generics
     let _s = Single(A);
@@ -329,4 +338,13 @@ fn main() {
     println!("first: {}", container.first());
     println!("last: {}", container.last());
     println!("the diffdrence is {}", difference(&container));
+
+    // phantom type parameters 
+    let _tuple1: PhantomTuple<char, f32> = PhantomTuple('Q', PhantomData);
+    let _tuple2: PhantomTuple<char, f64> = PhantomTuple('Q', PhantomData);
+
+    let _struct1: PhantomStruct<char, f32> = PhantomStruct { first: 'Q', phantom: PhantomData };
+    let _struct2: PhantomStruct<char, f64> = PhantomStruct { first: 'Q', phantom: PhantomData };
+    
+
 }
