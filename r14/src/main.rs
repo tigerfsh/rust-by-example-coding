10f685c7514d709fd47c6206ac8cf85469023de7
fushouhai <fushouhai@haier.com>
2023-05-17 20:05:32 +0800

    update associated types

--- r14/src/main.rs
+++ r14/src/main.rs
@@ -173,21 +173,56 @@ fn old_enough(age: &Years) -> bool {
 }
 
 // associated types 
+// struct Container(i32, i32);
+
+// trait Contains<A, B> {
+//     fn contains(&self, _: &A, _: &B) -> bool;
+//     fn first(&self) -> i32;
+//     fn last(&self) -> i32;
+// }
+
+// impl Contains<i32, i32> for Container {
+//     fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
+//         (&self.0 == number_1) && (&self.1 == number_2) 
+//     }
+
+//     fn first(&self) -> i32 {
+//         self.0 
+//     }
+
+//     fn last(&self) -> i32 {
+//         self.1
+//     }
+// }
+
+// fn difference<A, B, C>(container: &C) -> i32 
+// where
+// C: Contains<A, B>
+// {
+//     container.last() - container.first()
+// }
+
 struct Container(i32, i32);
 
-trait Contains<A, B> {
-    fn contains(&self, _: &A, _: &B) -> bool;
+trait Contains {
+    type A;
+    type B;
+
+    fn contains(&self, _: &Self::A, _: &Self::B) -> bool;
     fn first(&self) -> i32;
     fn last(&self) -> i32;
 }
 
-impl Contains<i32, i32> for Container {
-    fn contains(&self, number_1: &i32, number_2: &i32) -> bool {
-        (&self.0 == number_1) && (&self.1 == number_2) 
+impl Contains for Container {
+    type A = i32;
+    type B = i32;
+
+    fn contains(&self, number_1: &Self::A, number_2: &Self::B) -> bool {
+        (&self.0 == number_1) && (&self.1 == number_2)
     }
 
     fn first(&self) -> i32 {
-        self.0 
+        self.0
     }
 
     fn last(&self) -> i32 {
@@ -195,13 +230,9 @@ impl Contains<i32, i32> for Container {
     }
 }
 
-fn difference<A, B, C>(container: &C) -> i32 
-where
-C: Contains<A, B>
-{
+fn difference<C: Contains>(container: &C) -> i32 {
     container.last() - container.first()
 }
-
 fn main() {
     // Generics
     let _s = Single(A);
