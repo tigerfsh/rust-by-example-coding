6fd2d6d3156eca5e5069ea8446fad2e4fcdee0b6
fushouhai <fushouhai@haier.com>
2023-08-21 18:16:46 +0800

    move 16.2 to r16

--- examples/r15/src/main.rs
+++ examples/r15/src/main.rs
@@ -255,7 +255,40 @@ fn main() {
 
     print_it(&NUM);
 
+    // 16.2
+    let random_number = 0.234;
+    let animal = random_animal(random_number);
+    print!("You've randomly chosen an animal, and it says {}", animal.noise());
+    
 }
+// 16.2 
+struct Sheep {}
+struct Cow {}
+
+trait Animal {
+    fn noise(&self) -> &'static str;
+}
+
+impl Animal for Sheep {
+    fn noise(&self) -> &'static str {
+        "baaaaaah!"
+    }
+}
+
+impl Animal for Cow {
+    fn noise(&self) -> &'static str {
+        "moooooooo!"
+    }        
+}
+
+fn random_animal(random_number: f64) -> Box<dyn Animal> {
+    if random_number < 0.5 {
+        Box::new(Sheep {})
+    } else {
+        Box::new(Cow {})
+    }
+}
+
 static NUM: i32 = 18;
 
 // 15.4.1 
