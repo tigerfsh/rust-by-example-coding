1c7b1226c4aa790adc08e9667392821552dfef3d
fushouhai <fushouhai@haier.com>
2023-08-22 17:59:12 +0800

    16.4

--- examples/r16/src/main.rs
+++ examples/r16/src/main.rs
@@ -227,9 +227,38 @@ fn main() {
 
     // 16.3 operator overloading
     println!("Foo + Bar = {:?}", Foo + Bar);
-}
-// 16.3
 
+    // 16.4
+    let _a = Droppable { name: "a" };
+
+    {
+        let _b = Droppable { name: "b" };
+        {
+            let _c = Droppable { name: "c" };
+            let _d = Droppable { name: "d" };
+            println!("Exiting block B");
+        }
+        println!("Just exited block B");
+
+        println!("Exiting block A");
+    }
+    println!("Just exited block A");
+
+    // drop(_a);
+    println!("end of the main func");
+}
+// 16.4
+struct Droppable {
+    name: &'static str,
+}
+
+impl Drop for Droppable {
+    fn drop(&mut self) {
+        println!("> Dropping {}", self.name);
+    }
+}
+
+// 16.3
 use std::ops::Add;
 
 struct Foo;
