931367b574d4717f0d84aef6c66778d97bd2a206
fushouhai <fushouhai@haier.com>
2023-08-23 17:51:59 +0800

    16.5

--- examples/r16/src/main.rs
+++ examples/r16/src/main.rs
@@ -246,7 +246,58 @@ fn main() {
 
     // drop(_a);
     println!("end of the main func");
+
+    // 16.5 Iterator 
+    let mut sequence = 0..3;
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+    println!("> {:?}", sequence.next());
+
+    for i in 0..3 {
+        println!("> {}", i);
+    }
+
+
+    println!("the first four terms: ");
+    for i in fibonacci().take(4) {
+        println!("> {}", i);
+    }
+
+    println!("skip four terms: ");
+    for i in fibonacci().skip(4).take(4) {
+        println!("> {}", i);
+    }
+
+    let array = [1u32, 3, 3, 7];
+    println!("array: {:?}", &array);
+    for i in array.iter() {
+        println!("> {}", i);
+    }
 }
+// 16.5 
+struct Fibonacci {
+    curr: u32, 
+    next: u32,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.curr;
+
+        self.curr = self.next;
+        self.next = current + self.next;
+
+        Some(current)
+    }
+}
+
+fn fibonacci() -> Fibonacci {
+    Fibonacci { curr: 0, next: 1 }
+}
+
 // 16.4
 struct Droppable {
     name: &'static str,
