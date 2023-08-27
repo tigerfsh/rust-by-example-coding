c98fc5017b96eea381fa8b2deb081de561d64240
tigerfsh <mrsmish@yahoo.com>
2023-08-27 11:53:06 +0800

    18.2

--- examples/r18/src/main.rs
+++ examples/r18/src/main.rs
@@ -1,13 +1,32 @@
+
+
+fn main() {
+    // 18.1
+
+    drink("water");
+    // drink("lemonade");
+
+    drink_v2("lemonade");
+}
+
+// 18.2
+// rustc  lemonade.rs -C panic=abort
+
+#[cfg(panic = "unwind")]
+fn ah(){ println!("Spit it out!!!!");}
+
+#[cfg(not(panic="unwind"))]
+fn ah(){ println!("This is not your party. Run!!!!");}
+
+fn drink_v2(beverage: &str){
+    if beverage == "lemonade"{ ah();}
+    else{println!("Some refreshing {} is all I need.", beverage);}
+}
+
+// 18.1
 fn drink(beverage: &str) {
     // You shouldn't drink too much sugary beverages.
     if beverage == "lemonade" { panic!("AAAaaaaa!!!!"); }
 
     println!("Some refreshing {} is all I need.", beverage);
-}
-
-fn main() {
-    // 18.1
-    
-    drink("water");
-    // drink("lemonade");
 }
