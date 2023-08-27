8065532afd94be87308ce555b6a49d93e6f1c013
tigerfsh <mrsmish@yahoo.com>
2023-08-27 18:02:13 +0800

    18.3

--- examples/r18/src/main.rs
+++ examples/r18/src/main.rs
@@ -8,6 +8,23 @@ fn main() {
 
     drink_v2("lemonade");
 }
+// 18.3 
+fn give_adult(drink:Option<&str>) {
+    match drink {
+        Some("lemonade") => println!("Yuck! Too sugary"),
+        Some(inner) => println!("{}? How nice.", inner),
+        None => println!("No drink ? oh, well."),
+    }
+}
+
+fn drink_v3(drink:Option<&str>) {
+    let inside = drink.unwrap();
+    if inside == "lemonade" {
+        panic!("AAAaaa");
+    }
+
+    println!("I love {}s!!!", inside);
+}
 
 // 18.2
 // rustc  lemonade.rs -C panic=abort
