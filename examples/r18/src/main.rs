8dc7a12f3bae863073f27e0e9799a54f5206cbf3
fushouhai <fushouhai@haier.com>
2023-08-29 16:22:19 +0800

    18.4.1

--- examples/r18/src/main.rs
+++ examples/r18/src/main.rs
@@ -55,6 +55,46 @@ fn main() {
 
     let first_available_fruit = my_fruit.get_or_insert_with(get_lemon_as_fallback_v2);
     println!("first_available_fruit: {:?}", first_available_fruit);
+
+    let twenty = multiply("10", "2");
+    print(twenty);
+
+    let tt = multiply("t", "2");
+    print(tt);
+}
+// 18.4.1
+use std::num::ParseIntError;
+
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    match first_number_str.parse::<i32>() {
+        Ok(first_number) => match second_number_str.parse::<i32>() {
+            Ok(second_number) => Ok(first_number * second_number),
+            Err(e) => Err(e),
+        },
+        Err(e) => Err(e),
+    }
+}
+
+fn multiply_v2(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str
+            .parse::<i32>()
+            .map(|second_number| first_number * second_number)
+    })
+}
+
+fn multiply_v3(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    first_number_str.parse::<i32>().and_then(|first_num| {
+        second_number_str
+            .parse::<i32>()
+            .map(|second_num| first_num * second_num)
+    })
+}
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n) => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
 }
 // 18.3.4
 #[derive(Debug)]
