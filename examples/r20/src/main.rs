58efa4d01bbf8c4829c6b4e6441c435ebb05dc9a
fushouhai <fushouhai@haier.com>
2023-09-14 22:08:07 +0800

    fmt .

--- examples/r20/src/main.rs
+++ examples/r20/src/main.rs
@@ -1,7 +1,7 @@
-use std::thread;
-use std::sync::Arc;
-use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::sync::Arc;
+use std::thread;
 
 // static NTHREADS: i32 = 3;
 
@@ -53,25 +53,25 @@ fn main() {
 
     // we use the "turbofish" ::<> to provide sum() with a type hint.
     // let final_result = children.into_iter().map(|c| c.join().unwrap()).sum::<u32>();
-    
+
     // try without the turbofish, by instead explicitly
     // specifying the type of final_result
     let final_result: u32 = children.into_iter().map(|c| c.join().unwrap()).sum();
-    
+
     println!("(A) Final sum result: {}", final_result);
 
-    // 20.1.1 
+    // 20.1.1
     // Limit thread nums
-    // split input 
+    // split input
     // 20.1.1
 
     /*
-    Note that, although we're passing references across thread boundaries, 
-    Rust understands that we're only passing read-only references, and 
-    that thus no unsafety or data races can occur. Also because the references 
-    we're passing have 'static lifetimes<how to understand this?>, Rust understands 
-    that our data won't be destroyed while these threads are still running. 
-    (When you need to share non-static data between threads, you can use a smart 
+    Note that, although we're passing references across thread boundaries,
+    Rust understands that we're only passing read-only references, and
+    that thus no unsafety or data races can occur. Also because the references
+    we're passing have 'static lifetimes<how to understand this?>, Rust understands
+    that our data won't be destroyed while these threads are still running.
+    (When you need to share non-static data between threads, you can use a smart
     pointer like Arc to keep the data alive and avoid non-static lifetimes.)
     */
     let data = "86967897737416471853297327050364959
@@ -83,7 +83,7 @@ fn main() {
 69920216438980873548808413720956532
 16278424637452589860345374828574668";
     const NUM: usize = 2;
-    
+
     let chunked_data = data.split_whitespace();
     let chunked_list: Vec<&str> = chunked_data.collect();
     let mut final_result = 0;
@@ -91,19 +91,15 @@ fn main() {
     for i in (0..chunked_list.len()).step_by(NUM) {
         let mut children = vec![];
 
-        let chunked_list = &chunked_list[i..(i+NUM)];
+        let chunked_list = &chunked_list[i..(i + NUM)];
         for &data_segment in chunked_list.iter() {
             children.push(thread::spawn(move || -> u32 {
-                let result = data_segment
-                    .chars()
-                    .map(|c| c.to_digit(10).unwrap())
-                    .sum();
+                let result = data_segment.chars().map(|c| c.to_digit(10).unwrap()).sum();
                 result
-
             }));
         }
-        let tmp_result: u32  = children.into_iter().map(|h| h.join().unwrap()).sum();
-        
+        let tmp_result: u32 = children.into_iter().map(|h| h.join().unwrap()).sum();
+
         final_result += tmp_result;
     }
     println!("(B) Final sum result: {}", final_result);
@@ -117,9 +113,6 @@ fn main() {
         GLOBAL_COUNTER += 1;
         println!("GLOBAL_COUNTER: {GLOBAL_COUNTER}");
     }
-    
 
     // 20.2 channel
-
 }
- 
