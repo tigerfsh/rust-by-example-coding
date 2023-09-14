cd3370268259012fc7fa76ded8c0405ad03eee79
fushouhai <fushouhai@haier.com>
2023-09-15 00:24:27 +0800

    20.4.2

--- examples/r20/src/main.rs
+++ examples/r20/src/main.rs
@@ -5,6 +5,7 @@ use std::sync::Arc;
 use std::thread;
 
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 // static NTHREADS: i32 = 3;
@@ -181,4 +182,32 @@ fn main() {
         Err(why) => panic!("can not read {}: {}", display, why),
         Ok(_) => println!("{} contains: \n{}", display, s),
     }
+
+    // 20.4.2 create file
+    let path = Path::new("lorem_ipsum.txt");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Ok(file) => file,
+        Err(why) => panic!("can not create {}:{}", display, why),
+    };
+
+    match file.write_all(LOREM_IPSUM.as_bytes()) {
+        Err(why) => panic!("can not write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("lorem_ipsum.txt")
+        .unwrap();
+    file.write_all("AAABBBCCC\n".as_bytes()).unwrap();
 }
+
+static LOREM_IPSUM: &str =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+";
