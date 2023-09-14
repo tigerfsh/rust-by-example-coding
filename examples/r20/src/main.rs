f1ef116d5decb1393f4cce12b6b27618bfc975a4
fushouhai <fushouhai@haier.com>
2023-09-14 10:19:55 +0800

    20.1.1

--- examples/r20/src/main.rs
+++ examples/r20/src/main.rs
@@ -3,6 +3,7 @@ use std::thread;
 const NTHREADS: u32 = 10;
 
 fn main() {
+    // 20.1
     let mut children = vec![];
     for i in 0..NTHREADS {
         children.push(thread::spawn(move || {
@@ -13,4 +14,34 @@ fn main() {
     for child in children {
         let _ = child.join();
     }
+
+    // 20.1.1
+    let data = "86967897737416471853297327050364959
+11861322575564723963297542624962850
+70856234701860851907960690014725639
+38397966707106094172783238747669219
+52380795257888236525459303330302837
+58495327135744041048897885734297812
+69920216438980873548808413720956532
+16278424637452589860345374828574668";
+
+    let mut children = vec![];
+
+    let chunked_data = data.split_whitespace();
+    for (i, data_segment) in chunked_data.enumerate() {
+        println!("data segment {} is \"{}\"", i, data_segment);
+
+        children.push(thread::spawn(move || -> u32 {
+            let result = data_segment
+                .chars()
+                .map(|c| c.to_digit(10).expect("should be a digit"))
+                .sum();
+            println!("processed segment {}, result={}", i, result);
+
+            result
+        }));
+    }
+
+    let final_result = children.into_iter().map(|c| c.join().unwrap()).sum::<u32>();
+    println!("Final sum result: {}", final_result);
 }
