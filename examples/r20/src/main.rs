96f8055ada873e8c129f925ebede76b78874b01a
fushouhai <fushouhai@haier.com>
2023-09-20 00:28:09 +0800

    20.6

--- examples/r20/src/main.rs
+++ examples/r20/src/main.rs
@@ -4,6 +4,7 @@ use std::sync::mpsc::{Receiver, Sender};
 use std::sync::Arc;
 use std::thread;
 
+use std::fs;
 use std::fs::read_to_string;
 use std::fs::File;
 use std::fs::OpenOptions;
@@ -13,6 +14,7 @@ use std::io::{self};
 
 use std::process::Command;
 use std::process::Stdio;
+use std::os::unix;
 
 static PANGRAM: &'static str = "Welcome to Beijing\n";
 
@@ -261,6 +263,67 @@ fn main() {
     let _result = child.wait().unwrap();
 
     println!("done.");
+
+    // 20.6
+    println!("`mkdir a`");
+    // Create a directory, returns `io::Result<()>`
+    match fs::create_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(_) => {},
+    }
+
+    println!("`echo hello > a/b.txt`");
+    // The previous match can be simplified using the `unwrap_or_else` method
+    echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`mkdir -p a/c/d`");
+    // Recursively create a directory, returns `io::Result<()>`
+    fs::create_dir_all("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`touch a/c/e.txt`");
+    touch(&Path::new("a/c/e.txt")).unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`ln -s ../b.txt a/c/b.txt`");
+    // Create a symbolic link, returns `io::Result<()>`
+    if cfg!(target_family = "unix") {
+        unix::fs::symlink("../b.txt", "a/c/b.txt").unwrap_or_else(|why| {
+            println!("! {:?}", why.kind());
+        });
+    }
+
+    println!("`cat a/c/b.txt`");
+    match cat(&Path::new("a/c/b.txt")) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(s) => println!("> {}", s),
+    }
+
+    println!("`ls a`");
+    // Read the contents of a directory, returns `io::Result<Vec<Path>>`
+    match fs::read_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => for path in paths {
+            println!("> {:?}", path.unwrap().path());
+        },
+    }
+
+    println!("`rm a/c/e.txt`");
+    // Remove a file, returns `io::Result<()>`
+    fs::remove_file("a/c/e.txt").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
+    println!("`rmdir a/c/d`");
+    // Remove an empty directory, returns `io::Result<()>`
+    fs::remove_dir("a/c/d").unwrap_or_else(|why| {
+        println!("! {:?}", why.kind());
+    });
+
 }
 
 // 20.4.3
@@ -270,6 +333,25 @@ fn read_lines(filename: &str) -> Vec<String> {
         result.push(line.to_string());
     }
     result
+}
+// 20.6
+fn cat(path: &Path) -> io::Result<String> {
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn echo(s: &str, path: &Path) -> io::Result<()> {
+    let mut f = File::create(path)?;
+    f.write_all(s.as_bytes())
+}
+
+fn touch(path: &Path) -> io::Result<()> {
+    match OpenOptions::new().create(true).write(true).open(path) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
+    }
 }
 
 fn read_lines_v2(filename: &str) -> Vec<String> {
