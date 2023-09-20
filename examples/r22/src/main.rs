06d32bc6eb0586005560a631ac7daa0cbbb63343
fushouhai <fushouhai@haier.com>
2023-09-20 21:46:53 +0800

    22

--- examples/r22/src/main.rs
+++ examples/r22/src/main.rs
@@ -1,3 +1,28 @@
+/*
+ Unsafe annotations in Rust are used to bypass protections put in place by the compiler; specifically, there are four primary things that unsafe is used for:
+
+    dereferencing raw pointers
+    calling functions or methods which are unsafe (including calling a function over FFI, see a previous chapter of the book)
+    accessing or modifying static mutable variables
+    implementing unsafe traits
+
+
+*/
+use std::slice;
+
 fn main() {
-    println!("Hello, world!");
+    let raw_p: *const u32 = &10;
+
+    unsafe {
+        assert!(*raw_p == 10);
+    }
+
+    let some_vector = vec![1, 2, 3, 4];
+    let pointer = some_vector.as_ptr();
+    let length = some_vector.len();
+
+    unsafe {
+        let my_slice: &[u32] = slice::from_raw_parts(pointer, length);
+        assert_eq!(some_vector.as_slice(), my_slice);
+    }
 }
